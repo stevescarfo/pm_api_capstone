c8aac52395cbc5067e1a4db0b92ce992bfe835bc
stevescarfo <steve@stevescarfo.com>
2020-10-26 15:44:52 -0400

    fixed POST push

--- routes/policyRouter.js
+++ routes/policyRouter.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // Policy Home page route.
 router.get('/', function (req, res) {
   res.send('Policy home page');
-})
+});
+
+router.post('/', function (req, res) {
+  res.json({ message: "Add a Policy", requestBody: req.body});
+});
 
 module.exports = router;
